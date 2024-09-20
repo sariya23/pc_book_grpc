57293cd6cd8e28a52604b4c283980b813b34b93a
sariya <dayzwaitik123@yandex.ru>
2024-09-20 17:38:32 +0300

    add server

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
-gen:
+gen_pb:
 	protoc -I ./proto proto/*.proto  --go_out=./pb/ --go_opt=paths=source_relative --go-grpc_out=./pb/ --go-grpc_opt=paths=source_relative
 
-clean:
+clean_pb:
 	rm pb/*.go
 
-run:
-	go run main.go
+server:
+	go run cmd/server/main.go -port 8080
+
+client:
+	go run cmd/client/main.go
 
 test:
 	go test -cover -race ./...
+
---
+++ cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"main/pb"
+	"main/service"
+	"main/storage"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("port", 0, "the server port")
+	flag.Parse()
+	log.Println("starting grpc server")
+	server := service.NewLaptopServer(storage.NewInMemoryLaptopStorage())
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, server)
+
+	addr := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("cannot start server: (%v)", err)
+	}
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("cannot start server")
+	}
+}
